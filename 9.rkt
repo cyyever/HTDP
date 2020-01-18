f5d10ed1138d866d5dc6e3233a3b4746be567505
cyy <cyyever@outlook.com>
2020-01-18 14:35:02 +0800

    add code

---
+++ 9.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |9|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image) 
+(define in 5)
+(cond
+  ((string? in) (string-length in))
+  ((image? in) (* (image-height in) (image-width in))) 
+  ((number? in)
+   (if (> in 0) (- in 1) in))
+  ((boolean? in) (if in 10 20)))
