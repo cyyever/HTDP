291501cef04c8cb5be19dfb63d8675ee1a3217a0
cyy <cyyever@outlook.com>
2020-10-25 17:39:22 +0700

    add code

--- 27_28.rkt
+++ 27_28.rkt
@@ -2,22 +2,30 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-(define base_people 120)
-(define base_ticket_price 5.0)
-(define people_increasing_amount 15)
-(define price_change 0.1)
+(define BASE-PEOPLE 120)
+(define BASE-TICKET-PRICE 5.0)
+(define PEOPLE-INCREASING-AMOUNT 15)
+(define PRICE-CHANGE 0.1)
 (define (attendees ticket-price)
-    (- base_people (* (- ticket-price base_ticket_price) (/ people_increasing_amount price_change))))
+    (- BASE-PEOPLE (* (- ticket-price BASE-TICKET-PRICE) (/ PEOPLE-INCREASING-AMOUNT PRICE-CHANGE))))
 
 (define (revenue ticket-price)
     (* ticket-price (attendees ticket-price)))
 
 
-(define performance_cost 180)
-(define attendee_cost 0.04)
+(define FIXED-COST 180)
+(define ATTENDEE-COST 0.04)
 (define (cost ticket-price)
-    (+ performance_cost (* attendee_cost (attendees ticket-price))))
+    (+ FIXED-COST (* ATTENDEE-COST (attendees ticket-price))))
 
 (define (profit ticket-price)
     (- (revenue ticket-price)
             (cost ticket-price)))
+
+(profit 1)
+(profit 2)
+(profit 3)
+(profit 2.9)
+(profit 2.8)
+(profit 4)
+(profit 5)
--- 29.1.rkt
+++ 29.1.rkt
@@ -2,20 +2,20 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-(define base_people 120)
-(define base_ticket_price 5.0)
-(define people_increasing_amount 15)
-(define price_change 0.1)
+(define BASE-PEOPLE 120)
+(define BASE-TICKET-PRICE 5.0)
+(define PEOPLE-INCREASING-AMOUNT 15)
+(define PRICE-CHANGE 0.1)
 (define (attendees ticket-price)
-    (- base_people (* (- ticket-price base_ticket_price) (/ people_increasing_amount price_change))))
+    (- BASE-PEOPLE (* (- ticket-price BASE-TICKET-PRICE) (/ PEOPLE-INCREASING-AMOUNT PRICE-CHANGE))))
 
 (define (revenue ticket-price)
     (* ticket-price (attendees ticket-price)))
 
 
-(define attendee_cost 1.54)
+(define attendee-cost 1.54)
 (define (cost ticket-price)
-     (* attendee_cost (attendees ticket-price)))
+     (* attendee-cost (attendees ticket-price)))
 
 (define (profit ticket-price)
     (- (revenue ticket-price)
